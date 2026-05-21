97052f202adc35e015fc552499abbf966e1e8df0
Felicien <felicien.boury@edu.esiee-it.fr>
2026-05-21 11:07:06 +0200

    feature : added doors

--- checkpoint/checkpoint.gd
+++ checkpoint/checkpoint.gd
@@ -1,15 +1,11 @@
 extends Node2D
 class_name checkpoint
 @export var index : int = 1
+@onready var yay: AudioStreamPlayer2D = $yay
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var dict : Dictionary =SaveManager.load_game()
-	
-	var checkpointint:int = 0;
-	checkpointint = dict["checkpoint"];
-	print("checkpoint : ",checkpointint)
-	if checkpointint==index:
+	if getcheckpointint()==index:
 		Gamemanager.playervar.teleport(global_position.x,global_position.y)
 	
 	pass # Replace with function body.
@@ -19,8 +15,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func getcheckpointint() -> int : 
+	var dict : Dictionary =SaveManager.load_game()
+	var checkpointint:int = 0;
+	checkpointint = dict["checkpoint"];
+	return checkpointint
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
-		SaveManager.save_game({"checkpoint":index})
+		if getcheckpointint()!=index:
+			yay.play()
+			SaveManager.save_game({"checkpoint":index})
 		#print("checkpoint ! ")
